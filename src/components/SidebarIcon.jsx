e84f57e1192f5bf5fc7e30326d21a5655d743748
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-18 05:34:27 +0900

    ✨ impl sidebar with react, mui

---
+++ src/components/SidebarIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Icon from '../images/icon.png';
+
+export const SidebarIcon = () => {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} alt="" />
+      <p>programming@mail.com</p>
+    </div>
+  );
+};
